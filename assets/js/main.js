87b340807771b36219eead8a0357aa19c9a76532
Bagus Hary <bagushary79@gmail.com>
2024-08-09 11:38:48 +0700

    feat: create pagination on the main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const pokemonOffset = 0;
+let pokemonOffset = 0;
 const pokemonLimit = 30;
-const currentPage = 1;
+let currentPage = 1;
 
 const searchInput = document.querySelector(".search-input");
 const searchCloseIcon = document.querySelector(".search-close-icon");
@@ -10,14 +10,23 @@ const numberFilter = document.querySelector("#number");
 const listWrapper = document.querySelector(".list-wrapper");
 const notFoundMessage = document.querySelector("#not-found-message");
 
+const pagination = document.querySelector("#pagination");
+const nextButton = document.querySelector("#btn-next");
+const pokePage = document.querySelector("#poke-page");
+const previousButton = document.querySelector("#btn-previous");
+
 let allPokemons = [];
 
-fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pokemonOffset}&limit=${pokemonLimit}`)
-    .then((response) => response.json())
-    .then((data) => {
-        allPokemons = data.results;
-        displayPokemons(allPokemons);
-    });
+function fetchPokemon(pokemonOffset) {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pokemonOffset}&limit=${pokemonLimit}`)
+        .then((response) => response.json())
+        .then((data) => {
+            allPokemons = data.results;
+            displayPokemons(allPokemons);
+        });
+}
+
+fetchPokemon(pokemonOffset);
 
 async function fetchPokemonDataBeforeRedirect(id) {
     try {
@@ -33,24 +42,29 @@ async function fetchPokemonDataBeforeRedirect(id) {
     }
 }
 
+function capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+
 function displayPokemons(pokemon) {
     listWrapper.innerHTML = "";
 
     pokemon.forEach((pokemon) => {
         const pokemonID = pokemon.url.split("/")[6];
         const listItem = document.createElement("div");
+        const pokemonName = capitalizeFirstLetter(pokemon.name);
 
         listItem.className = "col-6 col-sm-4 col-md-3 col-lg-2 list-item"
 
         listItem.innerHTML = `
             <div class="card-custom rounded-3 shadow">
-                <div class="card-custom-number">${pokemonID}</div>
+                <div class="card-custom-number">#${pokemonID.padStart(3, "0")}</div>
 
                 <div class="card-custom-image">
-                    <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}">
+                    <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemonName}">
                 </div>
 
-                <div class="card-custom-name">${pokemon.name}</div>
+                <div class="card-custom-name">${pokemonName}</div>
             </div>
         `;
 
@@ -80,7 +94,7 @@ function handleSearch() {
         filteredPokemons = allPokemons.filter((pokemon) => {
             const pokemonID = pokemon.url.split("/")[6];
             return pokemonID.startsWith(searchTerm);
-        })
+        });
     } else {
         filteredPokemons = allPokemons;
     }
@@ -92,6 +106,12 @@ function handleSearch() {
     } else {
         notFoundMessage.classList.add("d-none");
     }
+
+    if (searchInput.value !== "") {
+        pagination.classList.add("d-none");
+    } else {
+        pagination.classList.remove("d-none");
+    }
 }
 
 searchCloseIcon.addEventListener("click", clearSearch);
@@ -101,4 +121,40 @@ function clearSearch() {
     searchCloseIcon.classList.add("d-none");
     displayPokemons(allPokemons);
     notFoundMessage.classList.add("d-none");
+    pagination.classList.remove("d-none");
+}
+
+pokePage.innerHTML = `Halaman ${currentPage} dari 10`;
+
+nextButton.addEventListener("click", handleNextButtonOnClick);
+previousButton.addEventListener("click", handlePreviousButtonOnClick);
+
+function handleNextButtonOnClick() {
+    currentPage += 1;
+    pokePage.innerHTML = `Halaman ${currentPage} dari 10`;
+
+    pokemonOffset += 30;
+    fetchPokemon(pokemonOffset);
+
+    previousButton.classList.remove("d-none");
+
+    if (currentPage === 10) {
+        nextButton.classList.add("d-none");
+    }
+}
+
+function handlePreviousButtonOnClick() {
+    currentPage -= 1;
+    pokePage.innerHTML = `Halaman ${currentPage} dari 10`;
+
+    pokemonOffset -= 30;
+    fetchPokemon(pokemonOffset);
+
+    if (currentPage < 10) {
+        nextButton.classList.remove("d-none");
+    }
+
+    if (currentPage === 1) {
+        previousButton.classList.add("d-none");
+    }
 }
